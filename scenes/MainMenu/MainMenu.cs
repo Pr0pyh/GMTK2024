ade37554b1b4d30df4385fbcd9b2e0d402da094c
Pr0pyh <andrej.drmanovic@gmail.com>
2024-08-17 18:05:35 +0200

    MainMenu

---
+++ scenes/MainMenu/MainMenu.cs
@@ -0,0 +1,14 @@
+using Godot;
+using System;
+
+public partial class MainMenu : Control
+{
+    public void _on_button_pressed()
+    {
+        GetTree().ChangeSceneToFile("res://scenes/World/World.tscn");
+    }
+    public void _on_button_2_pressed()
+    {
+        GetTree().Quit();
+    }
+}
