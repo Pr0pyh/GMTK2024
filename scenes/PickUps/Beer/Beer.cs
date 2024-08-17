3079ae47aedaf5edbb1d3f07b2db1d5eabbfac4b
Pr0pyh <andrej.drmanovic@gmail.com>
2024-08-17 14:30:53 +0200

    ZAJECARSKO

--- scenes/PickUps/Beer/Beer.cs
+++ scenes/PickUps/Beer/Beer.cs
@@ -4,9 +4,12 @@ using System;
 public partial class Beer : Area3D
 {
     public PickUpSpawner spawner;
+    AnimationPlayer animPlayer;
     public override void _Ready()
     {
         spawner = GetParent<PickUpSpawner>();
+        animPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+        animPlayer.Play("rolling");
     }
     public void _on_body_entered(Node3D body)
     {
